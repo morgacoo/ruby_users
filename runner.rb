a9b0c64dd76baf85bc2df2e6742761973a33a8c4
morgacoo <morgacoo@gmail.com>
2017-09-26 10:32:50 -0500

    saved users

--- runner.rb
+++ runner.rb
@@ -1,7 +1,7 @@
 require 'pp' #styles output better
 require_relative 'user' #use the user class I built and put in this folder
 
-user = User.new 'morgan@example.com, 'Morgan'
+user = User.new 'joe@example.com', 'Joe'
 
 pp user
 
